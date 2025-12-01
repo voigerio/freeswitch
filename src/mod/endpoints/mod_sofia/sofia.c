9dadb6efe0e2f038d8544b6dfa9ee513707fa99f
Rakesh <rakesh@voiger.io>
2025-12-01 15:31:31 +0400

    fig fix firing esl event on ping

--- src/mod/endpoints/mod_sofia/sofia.c
+++ src/mod/endpoints/mod_sofia/sofia.c
@@ -6577,8 +6577,8 @@ static void sofia_handle_sip_r_options(switch_core_session_t *session, int statu
 				if (switch_event_create_subclass(&event, SWITCH_EVENT_CUSTOM, MY_EVENT_PING) == SWITCH_STATUS_SUCCESS) {
 					switch_event_add_header_string(event, SWITCH_STACK_BOTTOM, "profile-name", profile->name);
 					switch_event_add_header_string(event, SWITCH_STACK_BOTTOM, "current-ping-status", "success");
-					switch_event_add_header_string(event, SWITCH_STACK_BOTTOM, "ping-time", "%d", ping_time);
-					switch_event_add_header_string(event, SWITCH_STACK_BOTTOM, "ping-count", "%d", sip_user_status.count);
+					switch_event_add_header(event, SWITCH_STACK_BOTTOM, "ping-time", "%d", ping_time);
+					switch_event_add_header(event, SWITCH_STACK_BOTTOM, "ping-count", "%d", sip_user_status.count);
 					switch_event_add_header_string(event, SWITCH_STACK_BOTTOM, "sip-user", sip->sip_to->a_url->url_user);
 					switch_event_add_header_string(event, SWITCH_STACK_BOTTOM, "sip-host", sip->sip_to->a_url->url_host);
 				}
